a074c58e23e95ee3512b4b0ff2327e9fd31f762a
Aaliyah Evans <44178825+aaliyah-e@users.noreply.github.com>
2026-02-12 14:35:23 -0600

    add files via upload

---
+++ lab4.php
@@ -0,0 +1,34 @@
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Aaliyah Evans</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<header>
+    <h1>Calculator</h1>
+    <h2>Aaliyah Evans</h2>
+</header>
+
+<p>
+    Enter two numbers in the spaces provided. Click the button to add them together.
+</p>
+
+<section class="calculator">
+<form action="lab4response.php" method="post" class="calculator-inputs">    
+    <input type="text" name="first-number"><br>
+    <input type="text" name="second-number"><br>
+<input type="submit">
+</form>
+</section>
+
+<p>
+    Figuring out how to add the two number inputs together from the form was challenging to me. 
+</p>
+</body>
+
+</html>
---
+++ lab4response.php
@@ -0,0 +1,21 @@
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Aaliyah Evans</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<header>
+<h1>Sum of two numbers</h1>
+<h2>Aaliyah Evans</h2>
+</header>
+
+The sum of your two numbers is <?php echo $_POST["first-number" + "second-number"]; ?>!
+
+</body>
+
+</html>
